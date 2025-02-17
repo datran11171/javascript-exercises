0c7c7ad4fdec6d85bdc900fd8fe9028fdecd169b
Jamie Le Tran <jamietran200@gmail.com>
2025-02-17 17:28:44 +1100

    Completed task 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(repeat, num) {
+    let result = '';
+    if (num < 0) {
+        return 'ERROR';
+    }
+    for (let i = 0; i < num; i++) {
+        result += repeat;
+    }
+    return result;
 
 };
 
