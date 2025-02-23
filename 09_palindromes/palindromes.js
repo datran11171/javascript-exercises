3f11ef0ce60cbcb0d881e106711ff33a504e484e
Jamie Le Tran <jamietran200@gmail.com>
2025-02-23 18:47:27 +1100

    completed palidromes task

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,15 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const cleanedWord = word
+        .toLoweredCase()
+        .split('')
+        .filter((character) => alphanumerical.includes(character))
+        .join('');
+    
+    const reversedWord = cleanedWord.split('').reverse().join('');
+
+    return cleanedWord === reversedWord;
+
 
 };
 
