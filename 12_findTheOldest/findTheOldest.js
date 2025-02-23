5845110c67a95ec6c80f98cea11f804bf0e01720
Jamie Le Tran <jamietran200@gmail.com>
2025-02-23 19:35:55 +1100

    Completed find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,17 @@
-const findTheOldest = function() {
+const getAge = function(death, birth) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+};
+
+const findTheOldest = function(array) {
+    return array.reduce((oldest, current) => {
+        const oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+        const currentAge = getAge(current.yearOfDeath, current.yearOfBirth);
+        return oldestAge < currentAge ? current : oldest;
+    });
+
 
 };
 
