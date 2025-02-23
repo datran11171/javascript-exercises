1182d83cb0e74cf9b6023999cc1d6912d927b5db
Jamie Le Tran <jamietran200@gmail.com>
2025-02-23 18:43:28 +1100

    Completed calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
+const add = function(x1, x2) {
+	return x1 + x2;
+};
+
+const subtract = function(x1, x2) {
+  return x1 - x2;
 	
 };
 
-const subtract = function() {
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0);
 	
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const multiply = function(x1, x2) {
+  return x1 * x2;
 
 };
 
-const power = function() {
+const power = function(x1, x2) {
+  return Math.pow(x1, x2);
 	
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let product = 1;
+  for (let i = x; i > 0; i--) {
+    product *= i;
+  }
 };
 
 // Do not edit below this line
